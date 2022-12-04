21ac18bda7d445930b7b35a87cd76c134c990117
Bill-Yeh <lawyer112240@gmail.com>
2022-12-04 21:18:30 +0800

    practice middleware and query, params, public

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); //__dirname 是node.js關鍵字，代表著目前 app.js所在的‵實體目錄‵喔！
+// app.use(express.static('public'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -37,18 +38,19 @@ app.use('/product', productRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    console.error('err:', err)
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+router.use(logger)
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('This is users page.');
+    console.log(req.body)
+    console.log(req.query)
+    console.log(req.query.names)
+    res.send('This is users page.');
 });
+
+router.get('/usersList', function(req, res, next) {
+    res.send('This is usersList page.');
+});
+
+router.get('/:id', (req, res) => {
+    console.log('users:', req.users)
+    console.log('params:', req.params)
+    res.send(`Get Users with ID ${req.params.id}`)
+})
+
+const user = [{ name: "Kobe" }, { name: "Jordan" }]
+router.param('id', (req, res, next, id) => {
+    req.users = user[id]
+    next()
+})
+
+function logger(req, res, next) {
+    console.log(req.originalUrl)
+    next()
+}
 
 module.exports = router;
